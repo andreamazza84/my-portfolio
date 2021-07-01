c9f0fe90b8f5c22c1a2f9b2c52387d5799a14e66
andreamazza84 <andrea.mazza84@gmail.com>
2021-07-01 11:44:41 +0200

    axios

--- src/components/BaseContact.vue
+++ src/components/BaseContact.vue
@@ -1,56 +1,86 @@
 <template>
   <form
+    id="contact-form"
     action="https://formsubmit.co/a263761003a53d0d3b44000bb483e046"
     method="POST"
+    @submit="submit()"
   >
     <v-row>
       <v-col>
         <v-text-field
+          v-model="message.name"
           class="form-input"
           flat
           label="Nome*"
           solo
           :error-messages="nameErrors"
-          @input="$v.name.$touch()"
-          @blur="$v.name.$touch()"
+          @input="$v.message.name.$touch()"
+          @blur="$v.message.name.$touch()"
         ></v-text-field>
       </v-col>
 
       <v-col cols="12">
         <v-text-field
+          v-model="message.email"
           class="form-input"
           flat
           label="Email*"
           solo
           :error-messages="emailErrors"
-          @input="$v.email.$touch()"
-          @blur="$v.email.$touch()"
+          @input="$v.message.email.$touch()"
+          @blur="$v.message.email.$touch()"
         ></v-text-field>
-        <input type="hidden" name="_next" :value="this.$store.state.baseURL + 'message-sent'">
       </v-col>
 
       <v-col cols="12">
         <v-text-field
+          v-model="message.object"
           class="form-input"
           flat
           label="Oggetto*"
           solo
           :error-messages="objectErrors"
-          @input="$v.object.$touch()"
-          @blur="$v.object.$touch()"
+          @input="$v.message.object.$touch()"
+          @blur="$v.message.object.$touch()"
         ></v-text-field>
       </v-col>
 
       <v-col cols="12">
         <v-textarea
+          v-model="message.text"
           class="form-input"
           flat
           label="Scrivi qua*"
           solo
           :error-messages="textErrors"
-          @input="$v.text.$touch()"
-          @blur="$v.text.$touch()"
+          @input="$v.message.text.$touch()"
+          @blur="$v.message.text.$touch()"
         ></v-textarea>
+      </v-col>
+
+      <v-col cols="12">
+        <v-checkbox
+          v-model="message.checkbox"
+          :error-messages="checkboxErrors"
+          label="Accetto termini e condizioni"
+          required
+          @change="$v.message.checkbox.$touch()"
+          @blur="$v.message.checkbox.$touch()"
+        >
+        </v-checkbox>
+      </v-col>
+
+      <v-col
+        class="mx-auto"
+        cols="auto"
+      >
+        <v-btn
+          class="btn-clear"
+          x-large
+          @click="clear()"
+        >
+          Reset
+        </v-btn>
       </v-col>
 
       <v-col
@@ -66,6 +96,7 @@
         </v-btn>
       </v-col>
     </v-row>
+    <input type="text" name="_honey" style="display:none" class="hidden-input">
   </form>
 </template>
 <script>
@@ -76,78 +107,97 @@
     mixins: [validationMixin],
 
     validations: {
-      name: { required, maxLength: maxLength(30) },
-      email: { required, email },
-      object: { required },
-      text: {required },
-      checkbox: {
-        checked (val) {
-          return val
+      message: {
+        name: { required, maxLength: maxLength(30) },
+        email: { required, email },
+        object: { required },
+        text: { required },
+        checkbox: {
+          checked (val) {
+            return val
+          },
         },
-      },
+      }
     },
 
     data: () => ({
-      name: '',
-      email: '',
-      select: null,
-      checkbox: false,
+      message:{
+        name: '',
+        email: '',
+        select: null,
+        text: '',
+        object: '',
+        checkbox: false,
+      }
     }),
 
     computed: {
       checkboxErrors () {
         const errors = []
-        if (!this.$v.checkbox.$dirty) return errors
-        !this.$v.checkbox.checked && errors.push('Devi accettare le condizioni per continuare')
+        if (!this.$v.message.checkbox.$dirty) return errors
+        !this.$v.message.checkbox.checked && errors.push('Devi accettare le condizioni per continuare')
         return errors
       },
       nameErrors () {
         const errors = []
-        if (!this.$v.name.$dirty) return errors
-        !this.$v.name.maxLength && errors.push('Richiesto')
-        !this.$v.name.required && errors.push('Richiesto')
+        if (!this.$v.message.name.$dirty) return errors
+        !this.$v.message.name.maxLength && errors.push('Richiesto')
+        !this.$v.message.name.required && errors.push('Richiesto')
         return errors
       },
       objectErrors () {
         const errors = []
-        if (!this.$v.object.$dirty) return errors
-        !this.$v.object.maxLength && errors.push('Richiesto')
-        !this.$v.object.required && errors.push('Richiesto')
+        if (!this.$v.message.object.$dirty) return errors
+        !this.$v.message.object.maxLength && errors.push('Richiesto')
+        !this.$v.message.object.required && errors.push('Richiesto')
         return errors
       },
       textErrors () {
         const errors = []
-        if (!this.$v.text.$dirty) return errors
-        !this.$v.text.maxLength && errors.push('Richiesto')
-        !this.$v.text.required && errors.push('Richiesto')
+        if (!this.$v.message.text.$dirty) return errors
+        !this.$v.message.text.maxLength && errors.push('Richiesto')
+        !this.$v.message.text.required && errors.push('Richiesto')
         return errors
       },
       emailErrors () {
         const errors = []
-        if (!this.$v.email.$dirty) return errors
-        !this.$v.email.email && errors.push('Inserisci un indirizzo email valido')
-        !this.$v.email.required && errors.push('Richiesto')
+        if (!this.$v.message.email.$dirty) return errors
+        !this.$v.message.email.email && errors.push('Inserisci un indirizzo email valido')
+        !this.$v.message.email.required && errors.push('Richiesto')
         return errors
       },
     },
 
     methods: {
-      submit () {
-        this.$v.$touch()
+      submit(message) {
+        this.$v.$touch();
+        
+        
       },
       clear () {
         this.$v.$reset()
-        this.name = ''
-        this.email = ''
-        this.select = null
-        this.checkbox = false
+        this.message.name = ''
+        this.message.email = ''
+        this.message.object = ''
+        this.message.select = null
+        this.message.checkbox = false
       },
     },
   }
 </script>
 <style lang="scss" scoped>
-  .btn-submit{
-    background-color: $b_triadic_2 !important;
-    color: $f_dark !important;
+  #contact-form{
+    position: relative;   
+    .hidden-input{
+      position: absolute;
+    }
+    .btn-submit{
+      background-color: $b_triadic_1 !important;
+      color: $f_dark !important;
+    }
+    .btn-clear{
+      background-color: $b_triadic_2 !important;
+      color: $f_dark !important;
+    }
   }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,11 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import Vuelidate from 'vuelidate'
+import VueClipboards from 'vue-clipboards'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import 'roboto-fontface/css/roboto-slab/roboto-slab-fontface.css'
 import 'font-awesome/css/font-awesome.css'
-import Vuelidate from 'vuelidate'
-import VueClipboards from 'vue-clipboards'
+
+Vue.use(VueAxios, axios)
 Vue.use(VueClipboards)
 Vue.use(Vuelidate)
 
